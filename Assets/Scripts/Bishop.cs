02796cac9d09770e52d1748ae49e048864814eaa
Seth Climenhaga <climense@sheridancollege.ca>
2020-07-28 13:41:20 -0400

    Base ChessMan Class Created: individual scripts applied, BoardManager: basic movement implemented

--- Assets/Scripts/Bishop.cs
+++ Assets/Scripts/Bishop.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bishop : MonoBehaviour
+public class Bishop : ChessMan
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using UnityEngine;  
+using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-
+    public ChessMan[,] Chessmans { set; get; }
+    private ChessMan selectedChessMan;
 
     private const float tile_Size = 1.0f;
     private const float tile_OffSet = 0.5f;
@@ -13,6 +14,8 @@ public class BoardManager : MonoBehaviour
     private int selectionY = -1;
 
     private Quaternion orientation = Quaternion.Euler(-90, -90, 0);
+
+    public bool isWhiteTurn = true;
 
     public List<GameObject> chessmanPrefabs;
     private List<GameObject> activeChessMan;
@@ -25,7 +28,52 @@ public class BoardManager : MonoBehaviour
     {
         UpdateSelection();
         DrawChessBoard();
-        
+
+ 
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("Mouse Clicked");
+            if (selectionX >= 0 && selectionY >= 0)
+            {
+                if (selectedChessMan == null)
+                {
+                    //select the chessman
+                    SelectChessMan(selectionX, selectionY);
+                    Debug.Log("SelectedChessMan == Null");
+                }
+                else
+                {
+                    //move the chess man
+                    MoveChessMan(selectionX, selectionY);
+                }
+            }
+        }
+
+    }
+    private void SelectChessMan(int x, int y)
+    {
+        if (Chessmans[x, y] == null)
+        {
+            Debug.Log("Chessmans[x, y] == null in SelectChess", Chessmans[x, y]);
+            return;
+        }
+
+        if (Chessmans[x, y].isWhite != isWhiteTurn)
+            return;
+        selectedChessMan = Chessmans[x, y];
+        Debug.Log("SelectChessMan Called", selectedChessMan);
+    }
+    private void MoveChessMan(int x, int y)
+    {
+        if (selectedChessMan.PossibleMove(x, y))
+        {
+            Chessmans[selectedChessMan.CurrentX, selectedChessMan.CurrentY] = null;
+            selectedChessMan.transform.position = GetTileCenter(x, y);
+            Chessmans[x, y] = selectedChessMan;
+        }
+
+        selectedChessMan = null;
     }
 
     private void UpdateSelection()
@@ -38,7 +86,7 @@ public class BoardManager : MonoBehaviour
         {
             selectionX = (int)hit.point.x;
             selectionY = (int)hit.point.z;
-            
+
         }
         else
         {
@@ -46,11 +94,13 @@ public class BoardManager : MonoBehaviour
             selectionY = -1;
         }
     }
-    private void SpawnChessMan(int index, Vector3 position)
+    private void SpawnChessMan(int index, int x, int y)
     {
 
-        GameObject go = Instantiate(chessmanPrefabs [index], position, orientation) as GameObject;
+        GameObject go = Instantiate(chessmanPrefabs[index], GetTileCenter(x, y), orientation) as GameObject;
         go.transform.SetParent(transform);
+        Chessmans[x, y] = go.GetComponent<ChessMan>();
+        Chessmans[x, y].SetPosition(x, y);
         activeChessMan.Add(go);
 
     }
@@ -58,47 +108,48 @@ public class BoardManager : MonoBehaviour
     private void SpawnAllChessMan()
     {
         activeChessMan = new List<GameObject>();
+        Chessmans = new ChessMan[8, 8];
 
         //Spawn White Team
         //King
-        SpawnChessMan(0, GetTileCenter(3, 0));
+        SpawnChessMan(0, 3, 0);
         //Queen
-        SpawnChessMan(1, GetTileCenter(4, 0));
+        SpawnChessMan(1, 4, 0);
         //Rooks
-        SpawnChessMan(2, GetTileCenter(0, 0));
-        SpawnChessMan(2, GetTileCenter(7, 0));
+        SpawnChessMan(2, 0, 0);
+        SpawnChessMan(2, 7, 0);
         //Bishops
-        SpawnChessMan(3, GetTileCenter(2, 0));
-        SpawnChessMan(3, GetTileCenter(5, 0));
+        SpawnChessMan(3, 2, 0);
+        SpawnChessMan(3, 5, 0);
         //Knights
-        SpawnChessMan(4, GetTileCenter(1, 0));
-        SpawnChessMan(4, GetTileCenter(6, 0));
-
-        //Pawns
-        for(int xTile = 0; xTile < 8; xTile++)
-        {
-            SpawnChessMan(5, GetTileCenter(xTile, 1));
-        }
-
-        //Spawn White Team
-        //King
-        SpawnChessMan(6, GetTileCenter(3, 7));
-        //Queen
-        SpawnChessMan(7, GetTileCenter(4, 7));
-        //Rooks
-        SpawnChessMan(8, GetTileCenter(0, 7));
-        SpawnChessMan(8, GetTileCenter(7, 7));
-        //Bishops
-        SpawnChessMan(9, GetTileCenter(2, 7));
-        SpawnChessMan(9, GetTileCenter(5, 7));
-        //Knights
-        SpawnChessMan(10, GetTileCenter(1, 7));
-        SpawnChessMan(10, GetTileCenter(6, 7));
+        SpawnChessMan(4, 1, 0);
+        SpawnChessMan(4, 6, 0);
 
         //Pawns
         for (int xTile = 0; xTile < 8; xTile++)
         {
-            SpawnChessMan(11, GetTileCenter(xTile, 6));
+            SpawnChessMan(5, xTile, 1);
+        }
+
+        //Spawn White Team
+        //King
+        SpawnChessMan(6, 3, 7);
+        //Queen
+        SpawnChessMan(7, 4, 7);
+        //Rooks
+        SpawnChessMan(8, 0, 7);
+        SpawnChessMan(8, 7, 7);
+        //Bishops
+        SpawnChessMan(9, 2, 7);
+        SpawnChessMan(9, 5, 7);
+        //Knights
+        SpawnChessMan(10, 1, 7);
+        SpawnChessMan(10, 6, 7);
+
+        //Pawns
+        for (int xTile = 0; xTile < 8; xTile++)
+        {
+            SpawnChessMan(11, xTile, 6);
         }
     }
 
@@ -128,16 +179,16 @@ public class BoardManager : MonoBehaviour
         }
 
         //Draw the Selection
-        if(selectionX >=0 && selectionY >= 0)
+        if (selectionX >= 0 && selectionY >= 0)
         {
             Debug.Log(selectionX);
             Debug.Log(selectionY);
             Debug.DrawLine(
-                Vector3.forward * selectionY + Vector3.right * selectionX, 
+                Vector3.forward * selectionY + Vector3.right * selectionX,
                 Vector3.forward * (selectionY + 1) + Vector3.right * (selectionX + 1));
 
             Debug.DrawLine(
-                Vector3.forward * (selectionY +1) + Vector3.right * selectionX,
+                Vector3.forward * (selectionY + 1) + Vector3.right * selectionX,
                 Vector3.forward * selectionY + Vector3.right * (selectionX + 1));
 
         }
