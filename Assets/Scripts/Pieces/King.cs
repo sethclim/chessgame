e344df55adc46bf49bb16a70da5789384111667e
Devanshu Sabnani <sabnanid@sheridancollege.ca>
2020-08-06 08:06:31 -0400

    King movement added. But possible check !move still remaining.

--- Assets/Scripts/Pieces/King.cs
+++ Assets/Scripts/Pieces/King.cs
@@ -4,5 +4,94 @@ using UnityEngine;
 
 public class King : ChessMan
 {
-   
+    public override float CurrentZ { get => base.CurrentZ; set => base.CurrentZ = value; }
+
+
+    public King() : base()
+    {
+        CurrentZ = 0.42f;
+    }
+
+    public override MoveType[,] PossibleMove()
+    {
+        MoveType[,] r = new MoveType[8, 8];
+        ChessMan c;
+
+        int i, j;
+
+        // Right
+        i = CurrentX;
+
+        i++;
+        if (i < 8) 
+        {
+            c = BoardManager.Instance.Chessmans[i, CurrentY];
+
+            if (c == null) 
+            {
+                r[i, CurrentY] = MoveType.canMove;
+            }
+            else if (c.isWhite != isWhite)
+            {
+                r[i, CurrentY] = MoveType.attack;
+            }
+        }
+
+        // Left
+        i = CurrentX;
+
+        i--;
+        if (i >= 0)
+        {
+            c = BoardManager.Instance.Chessmans[i, CurrentY];
+
+            if (c == null)
+            {
+                r[i, CurrentY] = MoveType.canMove;
+            }
+            else if (c.isWhite != isWhite)
+            {
+                r[i, CurrentY] = MoveType.attack;
+            }
+        }
+
+        // UP
+        j = CurrentY;
+
+        j++;
+        if (j < 8)
+        {
+            c = BoardManager.Instance.Chessmans[CurrentX, j];
+
+            if (c == null)
+            {
+                r[CurrentX, j] = MoveType.canMove;
+            }
+            else if (c.isWhite != isWhite)
+            {
+                r[CurrentX, j] = MoveType.attack;
+            }
+        }
+
+        // Down
+        j = CurrentY;
+
+        j--;
+        if (j >= 0)
+        {
+            c = BoardManager.Instance.Chessmans[CurrentX, j];
+
+            if (c == null)
+            {
+                r[CurrentX, j] = MoveType.canMove;
+            }
+            else if (c.isWhite != isWhite)
+            {
+                r[CurrentX, j] = MoveType.attack;
+            }
+        }
+
+        return r;
+
+    }
 }
