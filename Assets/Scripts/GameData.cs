74c6f8403288ed9c3583a4e9612fda68800e7fd6
Seth Climenhaga <climense@sheridancollege.ca>
2020-08-06 18:27:45 -0400

    resolved merge issues

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -14,36 +14,36 @@ public class GameData
     {
     }
 
-<<<<<<< HEAD
+
     public GameData(BoardManager boardManager)
     {
         isWhiteTurn = boardManager.IsWhiteTurn;
-        chessManToSave = boardManager.act;
+       // chessManToSave = boardManager.act;
 
 
     }
-=======
-    //    for (int i = 0; i < activeChessMan.Count; i++)
-    //    {
-    //        ChessMan chessMan = activeChessMan[i];
-    //        allPieceData[i , 0] = (int)chessMan.Name;
-    //        allPieceData[i, 1] = chessMan.CurrentX;
-    //        allPieceData[i, 2] = chessMan.CurrentY;
-    //        int isWhite = 0;
-    //        if (chessMan.isWhite)
-    //            isWhite = 1;
-    //        allPieceData[i, 3] = isWhite;
-    //    }
-    //}
 
-    //public GameData(BoardManager boardManager)
-    //{
-    //    isWhiteTurn = boardManager.IsWhiteTurn;
-    //    chessManToSave = boardManager.ActiveChessMan;
+   //for (int i = 0; i < activeChessMan.Count; i++)
+   //     {
+   //        ChessMan chessMan = activeChessMan[i];
+   //        allPieceData[i , 0] = (int)chessMan.Name;
+   //        allPieceData[i, 1] = chessMan.CurrentX;
+   //        allPieceData[i, 2] = chessMan.CurrentY;
+   //        int isWhite = 0;
+   //         if (chessMan.isWhite)
+   //             isWhite = 1;
+   //         allPieceData[i, 3] = isWhite;
+   //     }
+   // }
+
+   // public GameData(BoardManager boardManager)
+   // {
+   //     isWhiteTurn = boardManager.IsWhiteTurn;
+   //     chessManToSave = boardManager.ActiveChessMan;
 
 
-    //}
->>>>>>> 00b57d5cdc45ffee07ad8b6f08d262308b106e61
+   // }
+
 
 
 
